eb801865ee723fd8f52362cb22a52a7f3c7268e8
Stytan <stytanus@gmail.com>
2017-05-20 17:30:37 +0300

    start

--- DZ_5/Program.cs
+++ DZ_5/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DZ_5
 {
-    class Program
+    public enum ArticleType { Motherboard, VideoCard, CPU, RAM, Monitor }
+    public struct Article
     {
-        static void Main(string[] args)
+        public int id;
+        public string name;
+        public double price;
+        public ArticleType type;
+    }
+    public enum ClientType { VIP, Gold, Standart }
+    public struct Client
+    {
+        public int id;
+        public string name;
+        public string address;
+        public string tel;
+        public int zakazov;
+        public double summ;
+        public ClientType type;
+    }
+    public struct RequestItem
+    {
+        public Article anArticle;
+        public int count;
+    }
+    public enum PayType { Card, Bitcoin, Cash }
+    public struct Request
+    {
+        public int id;
+        public Client aClient;
+        public DateTime date;
+        public RequestItem[] Items;
+        public PayType payType;
+        public double summ
         {
+            get
+            {
+                double sum = 0;
+                foreach (RequestItem item in Items)
+                {
+                    sum += item.anArticle.price * item.count;
+                }
+                return sum;
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //TODO
         }
     }
 }
